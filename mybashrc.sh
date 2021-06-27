ab6bbb355bda40f34b20b7db4240a35707606952
Manuel Solorzano <eevmanu@users.noreply.github.com>
2021-06-27 12:29:56 -0500

    upadte instruction to validate if pyenv command is able to execute

--- mybashrc.sh
+++ mybashrc.sh
@@ -61,7 +61,8 @@ PATH="$PATH:$HOME/bin"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
+  # eval "$(pyenv init -)"
   # eval "$(pyenv virtualenv-init -)"
 fi
 # PYENV_VERSION
